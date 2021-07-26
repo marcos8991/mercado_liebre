3c25ddea9034a714cef9b68f63accc94da9e1f1e
MarcosZarate <agustin_8991@outlook.com>
2021-07-26 13:27:36 -0300

    agrega styles y imagenes

--- app.js
+++ app.js
@@ -9,9 +9,5 @@ app.use(express.static('public'));
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views','home.html')))
 
 
-
-
-
-
 app.listen(port,()=>console.log('servidor corriendo en el puerto' + port));
 
